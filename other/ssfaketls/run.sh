07e51a04643381a8e80ddeafd01609b3fa278d96
shandong.zou <shandong.zou@wholeren.com>
2026-03-26 22:59:46 +0800

    up

--- other/ssfaketls/run.sh
+++ other/ssfaketls/run.sh
@@ -1,2 +1,9 @@
+source /opt/hiddify-manager/common/utils.sh
+
+# In docker apply paths install.sh may be skipped, so ensure required binaries exist.
+if ! command -v obfs-server >/dev/null 2>&1 || ! command -v ss-server >/dev/null 2>&1; then
+	install_package shadowsocks-libev simple-obfs
+fi
+
 systemctl enable hiddify-ss-faketls.service
 systemctl restart hiddify-ss-faketls.service
